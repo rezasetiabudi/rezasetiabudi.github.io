f9224acbf14d2e405b1de73f5b85720a0a1349c6
rezasetiabudi <shinichisetiabudi@gmail.com>
2019-03-07 14:45:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ classApp.config(function($routeProvider){
     controller : 'detailCtrl3'
   })
   .when('/cv',{
-    templateUrl : 'cv.html'
+    templateUrl : 'CV_Reza_Setiabudi.pdf'
   })
 });
 
